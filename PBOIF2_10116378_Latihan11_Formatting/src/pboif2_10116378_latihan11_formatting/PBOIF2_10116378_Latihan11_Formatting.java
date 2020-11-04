a7f0054ff2a7734c093a59422339e68bab0ca6dd
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 03:08:37 +0700

    Add files via upload

---
+++ PBOIF2_10116378_Latihan11_Formatting/src/pboif2_10116378_latihan11_formatting/PBOIF2_10116378_Latihan11_Formatting.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116378_latihan11_formatting;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class PBOIF2_10116378_Latihan11_Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
